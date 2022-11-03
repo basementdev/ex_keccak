0847712f31b56310793f3fd1e9379de4105832d6
Maarten van Vliet <maartenvanvliet@users.noreply.github.com>
2022-11-03 11:58:01 +0100

    Do not target musl

--- lib/ex_keccak.ex
+++ lib/ex_keccak.ex
@@ -11,7 +11,16 @@ defmodule ExKeccak do
     crate: :exkeccak,
     base_url: "https://github.com/basementdev/ex_keccak/releases/download/v#{version}",
     force_build: System.get_env("RUSTLER_PRECOMPILATION_EX_KECCAK_BUILD") in ["1", "true"],
-    version: version
+    version: version,
+    targets: [
+      "aarch64-apple-darwin",
+      "x86_64-apple-darwin",
+      "x86_64-unknown-linux-gnu",
+      "arm-unknown-linux-gnueabihf",
+      "aarch64-unknown-linux-gnu",
+      "x86_64-pc-windows-msvc",
+      "x86_64-pc-windows-gnu"
+    ]
 
   def hash_256(_data), do: :erlang.nif_error(:nif_not_loaded)
 end
